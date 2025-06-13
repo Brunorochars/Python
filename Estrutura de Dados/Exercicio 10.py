c2891ea88775d0cf8f691d5749f8abf588da4367
Bruno Rocha <brunorochars@hotmail.com>
2025-06-13 19:09:25 -0300

    Atualização Exercicios

--- Estrutura de Dados/Exercicio 10.py
+++ Estrutura de Dados/Exercicio 10.py
@@ -1,32 +1,82 @@
-# Escrever um programa de um banco que permite criar 
-# contas, depositar e sacar dinheiro. Cada conta deve 
-# ter um número de conta, senha, nome do titular e 
-# saldo. O usuário deve poder se logar em um conta, 
-# realizar transações e visualizar o saldo. Usuários 
-# podem enviar dinheiro entre contas, e o programa deve 
+#   Escrever um programa de um banco que permite criar 
+# contas: 
+# =======================
+# Cada conta deve ter:
+#
+# - Número da conta
+# - Senha
+# - Nome do titular
+# - Saldo
+# =======================
+# O usuário de poder:
+#
+# - Criar conta
+# - Logar na conta
+# - Realizar transações
+# - Visualizar saldo
+# ========================
+#   Usuários podem enviar dinheiro entre contas, e o programa deve 
 # garantir que o saldo não fique negativo.
+# =============================================================================
 
-def criar_contas():
-    conta = {}
+def criar_conta(numero_de_contas_existentes):
 
-    conta["numero_conta"] = int(input("Digite o número da sua conta: "))
-    conta["senha"] = input("Digite uma senha: ")
-    conta["nome_titular"] = input("Digite o seu nome: ")
-    conta["saldo"] = float(input("Digite o saldo: "))
-
-    return conta
-
-contas = []
-
-conta_criada = criar_contas()
-
-conta = {"numero_conta":""}
-conta = {"senha":""}
-conta = {"nome_titular":""}
-conta = {"saldo": ""}
-
-while conta["saldo"] < 0:
+    nome_titular = input("Digite o seu nome: ")
+    senha = input("Digite a sua senha: ")
     
+    nova_conta = {
+
+        "numero_conta": numero_de_contas_existentes + 1,
+        "saldo":0,
+        "nome_titular": nome_titular,
+        "senha": senha,
+
+    }
+
+    return nova_conta
+
+def fazer_login(contas_existentes):
+
+    numero_conta =int(input("Digite o número da conta: "))
+    senha = input("Digite a senha: ")
+
+    for conta_existente in contas_existentes:
+
+        if numero_conta == conta_existente["numero"] and senha == conta_existente["senha"]:
+            return conta_existente
+
+def main():
+
+    contas = []
+
+    while True:
+
+        print("========== MENU ==========")
+        print()
+        print("1 - Criar conta") 
+        print("2 - Depositar/Sacar") 
+        print("3 - Sair")
+   
+        opcao_usuario = input("Digite a o opção desejada:")
+
+        if opcao_usuario == "3":
+            
+            print()
+            print("Até logo!!!")
+            print()
+            break 
+
+        if opcao_usuario == "1":
+            nova_conta = criar_conta(numero_de_contas_existentes=len(contas))
+            contas.append(nova_conta)
+
+        if opcao_usuario == "2":
+            conta_usuario = None
+            while conta_usuario is None:
+                fazer_login(contas_existentes=contas) ## Passa a lista de contas para contas_existentes
+        
+main()
+
     
 
 
